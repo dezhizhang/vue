bab924010066d37047c817aa3f522dd62b0860ea
dezhizhang <1541609448@qq.com>
2022-01-09 02:12:04 +0800

    feat:observer

---
+++ src/observer/index.js
@@ -0,0 +1,31 @@
+
+class Observer {
+    constructor(value) {
+        this.walk(value);
+    }
+
+    walk(target) {
+        Object.keys(target).forEach(key => {
+            defineReactive(target, key, target[key]);
+        })
+    }
+}
+
+function defineReactive(target, key, value) {
+    Object.defineProperty(target, key, {
+        get() {
+            return value;
+        },
+        set(newValue) {
+            if (newValue === value) return;
+            value = newValue;
+        }
+    })
+}
+
+export function observe(data) {
+    if (typeof data !== 'object' || data == null) {
+        return; // 不是对像不能观测
+    }
+    return new Observer(data);
+}
